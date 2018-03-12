805d4e17841c5c24c9f10d9634074113a097c22b
Subba <mandepudi@yahoo.com>
2018-03-12 18:52:57 +0000

    Updates - resource

--- recipes/server.rb
+++ recipes/server.rb
@@ -15,11 +15,35 @@ end
 #"
 #end
 
+#bash 'Inline Script' do
+#  user "root"
+#  code "mkdir -p /var/www/mysites/ && chown -R apache /var/www/mysites"
+#  not_if do
+#    File.directory?('/var/www/mysites');
+#  end
+#end
+
+directory "/var/www/mysites" do
+  owner 'apache'
+  recursive true
+  action :create
+end
+
+remote_file '/var/www/html/mahesh.png' do
+ source 'http://awallpapersimages.com/wp-content/uploads/2016/09/89.png'
+end
+
+cookbook_file '/var/www/html/cookbook_file_demo.html' do
+  source 'cbook_file.html'
+end
+
 template '/var/www/html/index.html' do
   source 'index.html.erb'
   action :create
+#  notifies :restart, 'service[httpd]' :immediately
 end
 
 service 'httpd' do
   action [:enable, :start]
+  subscribes :restart, 'template[/var/www/html/index.html]', :immediately
 end
